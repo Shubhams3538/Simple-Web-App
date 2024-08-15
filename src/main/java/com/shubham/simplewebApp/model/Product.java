7d8044205f8eb4c6234bbe174cd8247108561855
Shubham Singh <shubhams3538@gmail.com>
2024-08-15 08:29:24 +0530

    Added the H2 database functionality.

--- src/main/java/com/shubham/simplewebApp/model/Product.java
+++ src/main/java/com/shubham/simplewebApp/model/Product.java
@@ -1,17 +1,23 @@
 package com.shubham.simplewebApp.model;
 
-import java.util.Locale;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.springframework.stereotype.Component;
 
+@Component
+@Entity
 public class Product {
-
+    @Id
     private int prodID;
     private String prodName;
     private int price;
 
-    public Product(int prodID, String prodName , int price) {
+    public Product() {} // Default constructor needed by JPA
+
+    public Product(int prodID, String prodName, int price) {
         this.prodID = prodID;
-        this.price = price;
         this.prodName = prodName;
+        this.price = price;
     }
 
     public int getProdID() {
@@ -22,18 +28,19 @@ public class Product {
         this.prodID = prodID;
     }
 
+    public String getProdName() {
+        return prodName;
+    }
+
     public void setProdName(String prodName) {
         this.prodName = prodName;
-    }
-    public void setPrice(int price) {
-        this.price = price;
     }
 
     public int getPrice() {
         return price;
     }
 
-    public String getProdName() {
-        return prodName;
+    public void setPrice(int price) {
+        this.price = price;
     }
 }
